5c520ada54b7cc20e2c219980d750d078e14fdbf
Michael Georgariou III <mpgiii@ymail.com>
2018-10-11 17:04:05 -0700

    moved viewportToWorld and worldToViewport to Viewport class

--- src/Viewport.java
+++ src/Viewport.java
@@ -22,4 +22,14 @@ final class Viewport
       return p.y >= this.row && p.y < this.row + this.numRows &&
               p.x >= this.col && p.x < this.col + this.numCols;
    }
+
+   public Point viewportToWorld(int col, int row)
+   {
+      return new Point(col + this.col, row + this.row);
+   }
+
+   public Point worldToViewport(int col, int row)
+   {
+      return new Point(col - this.col, row - this.row);
+   }
 }
